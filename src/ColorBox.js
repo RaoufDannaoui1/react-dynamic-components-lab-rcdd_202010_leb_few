49eecc79ed3cbd8f31a07fa5f71894c4e8ba45ca
AbdelRaouf Dannaoui <123raoufdannaoui@gmail.com>
2021-01-19 14:15:53 +0000

    Automatically backed up by Learn

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 export default class ColorBox extends Component {
 
+<<<<<<< HEAD
   render() {
     if(this.props.opacity >= 0.2) {
       return (
@@ -12,6 +13,26 @@ export default class ColorBox extends Component {
     } else {
       return null
     }
+=======
+  state = {
+    todos: [
+
+    ]
   }
 
+  render() {
+    const op = this.props.opacity ;
+    if(op >= 0.2)
+    {
+      return
+      (
+      <div className="color-box" style={{opacity: op }}>
+        <ColorBox opacity={op} />
+      </div>
+      )
+    }
+    else
+    return null
+>>>>>>> a4422759f857b860267f4f301082086786af75a5
+  }
 }
--- src/Comment.js
+++ src/Comment.js
@@ -1,11 +1,21 @@
 //your code here
 import React, {Component} from 'react';
 
+<<<<<<< HEAD
 export default class Comment extends Component {
   render() {
     return (
       <div className="comment">
         {this.props.commentText}
+=======
+export default class ClassName extends Component {
+  render()
+  {
+    return
+    (
+      <div ClassName="comment" >
+      {this.props.commentText}
+>>>>>>> a4422759f857b860267f4f301082086786af75a5
       </div>
     )
   }
